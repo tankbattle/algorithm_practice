2a55e755a086726ad46c5b6c38ccb53c9a64b993
tankbattle <wuxiao19821021@gmail.com>
2023-05-27 15:38:33 -0700

    Add a solution for a new problem

---
+++ zoj/1910Blocks_ok.cpp
@@ -0,0 +1,44 @@
+#include <iostream>using namespace std;
+
+const int MAXSIZE = 1000;
+int MinArea[MAXSIZE+1];
+
+//Calculate the min area each count of block need
+void Init()
+{
+	int i;
+	//The min value is at most if they are in one line
+	for(i = 0 ; i <= MAXSIZE ; i ++)
+		MinArea[i] = i * 4 + 2;
+	//Calculate
+	for(i = 1 ; i <= MAXSIZE ; i ++)
+	{
+		int MaxJ = MAXSIZE / i;
+		for(int j = 1 ; j <= MaxJ ; j ++)
+		{
+			int MaxK = MaxJ / j;
+			for(int k = 1 ; k <= MaxK ; k ++)
+			{
+				int Size = i * j * k;
+				int Area = (i * j + j * k + k * i)*2;
+				if(Area < MinArea[Size])
+					MinArea[Size] = Area;
+			}
+		}
+	}
+	MinArea[0] = 0;
+}
+
+int main()
+{
+	Init();
+	int Test;
+	cin>>Test;
+	for(int i = 0; i < Test ; i ++)
+	{
+		int Size;
+		cin>>Size;
+		cout<<MinArea[Size]<<endl;
+	}
+	return 0;
+}
